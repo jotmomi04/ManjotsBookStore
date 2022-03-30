a592b86fb6739eea7bb01bab36ca23b40e67f189
jotmomi04 <w07779181@myscc>
2022-03-30 04:49:37 -0400

    Remodify startup

--- ManjotsBookStore/Startup.cs
+++ ManjotsBookStore/Startup.cs
@@ -36,7 +36,7 @@ namespace ManjotsBookStore
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
-            services.AddScoped<IUnitOfWork>();
+       
             services.AddControllersWithViews();
         }
 
