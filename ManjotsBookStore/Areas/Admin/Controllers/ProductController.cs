54e26b9cd1b04609ea50dc151c754fa6ebf6b259
jotmomi04 <91279286+jotmomi04@users.noreply.github.com>
2022-04-19 16:36:45 -0400

    update books

--- ManjotsBookStore/Areas/Admin/Controllers/ProductController.cs
+++ ManjotsBookStore/Areas/Admin/Controllers/ProductController.cs
@@ -10,7 +10,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BulkyBook.Areas.Admin.Controllers
+namespace ManjotsBooks.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class ProductController : Controller
